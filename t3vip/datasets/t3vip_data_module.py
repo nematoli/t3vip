2433dfdc12e5cec5ea112b121c2412f49e17dafb
Iman Nematollahi <nematollahi.iman@gmail.com>
2022-07-08 15:11:15 +0200

    handling test dataset

--- t3vip/datasets/t3vip_data_module.py
+++ t3vip/datasets/t3vip_data_module.py
@@ -29,7 +29,7 @@ class T3VIPDataModule(pl.LightningDataModule):
         super().__init__()
         self.train_dataset = None
         self.val_dataset = None
-        # self.test_dataset = None
+        self.test_dataset = None
 
         self.dataset = dataset
         root_data_path = Path(self.dataset.data_dir).expanduser()
@@ -46,12 +46,16 @@ class T3VIPDataModule(pl.LightningDataModule):
 
             self.train_episodes_info = root_data_path / "task_idx" / self.dataset.env / "training"
             self.val_episodes_info = root_data_path / "task_idx" / self.dataset.env / "validation"
+
+            self.test_dir = root_data_path / "task_D_D" / "validation"
+            self.test_episodes_info = root_data_path / "task_idx" / "env_d" / "validation"
+
         else:
             self.train_dir = root_data_path / "training"
             self.val_dir = root_data_path / "validation"
-            self.train_episodes_info = self.val_episodes_info = None
+            self.test_dir = root_data_path / "test"
+            self.train_episodes_info = self.val_episodes_info = self.test_episodes_info = None
 
-        # self.test_dir = root_data_path / "test"
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.resolution = resolution
@@ -88,18 +92,19 @@ class T3VIPDataModule(pl.LightningDataModule):
             xygrid=self.xygrid,
             ep_info=self.val_episodes_info,
         )
-        # self.test_dataset = hydra.utils.instantiate(
-        #     self.dataset,
-        #     data_dir=self.test_dir,
-        #     img_ht=self.resolution,
-        #     img_wd=self.resolution,
-        #     seq_len=self.seq_len,
-        #     step_len=self.step_len,
-        #     train=False,
-        #     transforms=self.transforms,
-        #     intrinsics=self.intrinsics,
-        #     xygrid=self.xygrid,
-        # )
+        self.test_dataset = hydra.utils.instantiate(
+            self.dataset,
+            data_dir=self.test_dir,
+            img_ht=self.resolution,
+            img_wd=self.resolution,
+            seq_len=self.seq_len,
+            step_len=self.step_len,
+            train=False,
+            transforms=self.transforms,
+            intrinsics=self.intrinsics,
+            xygrid=self.xygrid,
+            ep_info=self.test_episodes_info,
+        )
 
     def train_dataloader(self):
         return DataLoader(
@@ -121,12 +126,12 @@ class T3VIPDataModule(pl.LightningDataModule):
             drop_last=False,
         )
 
-    # def test_dataloader(self):
-    #     return DataLoader(
-    #         self.test_dataset,
-    #         batch_size=self.batch_size,
-    #         num_workers=self.num_workers,
-    #         pin_memory=False,
-    #         shuffle=False,
-    #         drop_last=False,
-    #     )
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            shuffle=False,
+            drop_last=False,
+        )
